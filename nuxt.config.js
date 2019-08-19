5bcb261c211ddeebc63e9846f18ee808c08a7773
simonppg <simonppg@gmail.com>
2019-08-19 14:09:04 -0700

    Change port form 8000 to process.env.PORT || 8000

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ module.exports = {
     extend(config, ctx) {}
   },
   server: {
-    port: 8000, // default: 3000
+    port: process.env.PORT || 8000, // default: 3000
     host: '0.0.0.0' // default: localhost
   }
 }
